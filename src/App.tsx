aa9482cccce2dbb39bc00bc221084543e2bfe161
22130010 <22130010@st.hcmuaf.edu.vn>
2025-12-24 19:32:26 +0700

    finish Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./componnents/Header";
 
 function App() {
   return (
-    <h1>hello</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ src/componnents/Header.tsx
@@ -0,0 +1,16 @@
+const  Header =()=>{
+    return (
+        <nav className="bg-white shadow-md p-4 flex justify-between items-center">
+            <h1 className="text-xl font-bold text-blue-700">Đồng Phục Happy</h1>
+            <ul className="flex space-x-5 text-sm text-gray-700">
+                <li>Trang Chủ</li>
+                <li>Sản Phẩm</li>
+                <li>Giỏ Hàng</li>
+                <li>Tin Tức</li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Header;
