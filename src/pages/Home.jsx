9032a2dc8a829ce6507421bf8252d35e6eb42e6a
22130010 <22130010@st.hcmuaf.edu.vn>
2025-12-29 15:27:42 +0700

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../componnents/Header";
+import Banner from "../componnents/Banner";
+import Footer from "../componnents/Footer";
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+            <Footer/>
+        </div>
+    );
+}
+export default Home;
