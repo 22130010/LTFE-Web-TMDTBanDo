055d70c50f0fdc41565b0177c9659f5e551c3fb9
22130010 <22130010@st.hcmuaf.edu.vn>
2026-01-03 10:14:55 +0700

    json

---
+++ src/componnents/SPnoibat.tsx
@@ -0,0 +1,146 @@
+const SPnoibat =()=>{
+    return (
+        <div className="bg-white">
+            <div className="max-w-6xl mx-auto my-12 px-4">
+
+                <div className="mb-8 text-center">
+                    <h2 className="text-2xl md:text-3xl font-bold mb-2">
+                        Sản Phẩm Nổi Bật
+                    </h2>
+                    <p className="text-gray-500">
+                        Cập nhật những sản phẩm nổi bật mỗi ngày
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu3EzNpw1lu3s_uvR5VtjXh7j3znehD06L_A&s"
+                            alt="Sản phẩm"
+                            className="w-full  object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu3EzNpw1lu3s_uvR5VtjXh7j3znehD06L_A&s"
+                            alt="Sản phẩm"
+                            className="w-full object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://via.placeholder.com/800x400"
+                            alt="Sản phẩm"
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://via.placeholder.com/800x400"
+                            alt="Sản phẩm"
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://via.placeholder.com/800x400"
+                            alt="Sản phẩm"
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+
+                    <div className="border rounded-lg overflow-hidden shadow-sm h-full">
+                        <img
+                            src="https://via.placeholder.com/800x400"
+                            alt="Tin tức"
+                            className="w-full h-48 object-cover"
+                        />
+                        <div className="p-4">
+                            <h3 className="text-lg font-semibold mb-2">
+                                Sản phẩm 1
+                            </h3>
+                            <p className="text-gray-600 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Repellat rerum sed ratione accusantium eaque,
+                                ad eligendi officia inventore magnam alias in assumenda
+                                delectus deleniti blanditiis.
+                                Quam delectus optio qui eum!
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    )
+};
+export default SPnoibat;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Header from "../componnents/Header";
 import Banner from "../componnents/Banner";
 import Footer from "../componnents/Footer";
+import SPnoibat from "../componnents/SPnoibat";
 
-function Home() {
+function Home()  {
     return (
         <div>
             <Header/>
             <Banner/>
+            <SPnoibat/>
             <Footer/>
         </div>
     );
