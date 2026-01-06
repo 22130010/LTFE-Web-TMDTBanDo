35cdd60c593362cd6172c889260396c9f9e0f41d
duypham <20130241@st.hcmuaf.edu.vn>
2026-01-06 21:40:41 +0700

    Merge branch 'main' into nhutduy
    trang giỏ hàng

---
+++ src/js/constant.js
@@ -0,0 +1,8 @@
+const dataMenu = [
+{ content: 'Home', href: "#"},
+{ content: 'Shop', href: "#"},
+{ content: 'Blog', href: "#"},
+{ content: 'About us', href: "#"},
+{ content: 'Contact us', href: "#"}
+];
+export { dataMenu };
