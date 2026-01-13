2d0b32da2c490157f64353934008b65b814c1a34
unknown <22130326@st.hcmuaf.edu.vn>
2026-01-13 14:49:03 +0700

    adding sort selection into productList

--- src/pages/SPaosomi.tsx
+++ src/pages/SPaosomi.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import productsData from "../services/Products.json";
-import {Link} from "react-router-dom";
+import ProductList from "./ProductList";
 
 interface Product {
     id: number;
@@ -15,56 +15,18 @@ interface Product {
 
 function SPaosomi() {
     const [products, setProducts] = useState<Product[]>([]);
-
     useEffect(() => {
         const aoSomiDongPhuc = productsData.filter(
             (item) => item.category === "Áo sơ mi công sở"
         );
-
         setProducts(aoSomiDongPhuc);
     }, []);
 
     return (
-
-        <div className="max-w-7xl mx-auto px-4 py-6">
-            <h1 className="text-3xl font-bold text-center mb-6">
-                Danh sách sản phẩm
-            </h1>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {products.map((item) => (
-                    <div
-                        key={item.id}
-                        className="border rounded-lg shadow hover:shadow-lg bg-white"
-                    >
-                        <img
-                            src={Array.isArray(item.image) ? item.image[0] : item.image}
-                            alt={item.name}
-                            className="w-full h-48 object-cover rounded-t-lg"
-                        />
-
-                        <div className="p-4">
-                            <h2 className="font-semibold text-sm line-clamp-2">
-                                {item.name}
-                            </h2>
-
-                            <p className="text-red-500 font-bold">
-                                {item.price.toLocaleString()} đ
-                            </p>
-
-                            <Link
-                                to={`/product/${item.id}`}
-                                className="block text-center w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
-                            >
-                                Xem chi tiết
-                            </Link>
-
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-
+        <ProductList
+            customTitle="Danh sách Áo Sơ Mi Công Sở"
+            searchResult={products}
+        />
     );
 }
 
